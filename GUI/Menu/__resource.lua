3e9b8500a317d246364be637dfe3b1f8dc5e2e6f
Nathan Agez <nathan.agez@gmail.com>
2017-04-13 11:08:44 +1100

    Update __resource.lua

--- GUI/Menu/__resource.lua
+++ GUI/Menu/__resource.lua
@@ -1,12 +1,13 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-ui_page "nui/menuPerso.html"
+ui_page "nui/index.html"
 
 files {
-	"nui/menuPerso.html",
-	"nui/menuPerso.js",
-	"nui/menuPerso.css",
+	"nui/index.html",
+	"nui/menu.js",
+	"nui/menu.css",
 	"nui/Roboto.ttf"
+	"nui/Los-Santos.ttf"
 }
 
 client_scripts {
